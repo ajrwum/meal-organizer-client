c670694968ba32c6623dff6aa9e7d33ae6a435e5
Mamadou AMADOU <doumstyle@gmail.com>
2022-02-07 13:01:07 +0100

    edit navigation links

--- src/components/Nav/NavMain.jsx
+++ src/components/Nav/NavMain.jsx
@@ -1,25 +1,27 @@
-import { NavLink } from 'react-router-dom';
-import useAuth from '../../auth/useAuth';
-import '../../styles/NavMain.css';
+import { NavLink } from "react-router-dom";
+import useAuth from "../../auth/useAuth";
+import "../../styles/NavMain.css";
 
 const NavMain = () => {
   const { isLoggedIn, currentUser, removeUser } = useAuth();
   return (
     <nav className="NavMain">
       <NavLink className="logo" to="/">
-        App name
+        MO
       </NavLink>
       {isLoggedIn && (
         <>
           <NavLink to="/profile">{currentUser && currentUser.email}</NavLink>
-          <NavLink to="/meals/meal/new">Create meal</NavLink>
-          <button onClick={removeUser}>Log-Out</button>
+          <NavLink to="/meals">Mes repas de la semaine</NavLink>
+          <NavLink to="/meals/meal/new">Créer un Repas</NavLink> // dev mode
+          <NavLink to="/foods">Mes Aliments</NavLink>
+          <button onClick={removeUser}>Déconnexion</button>
         </>
       )}
       {!isLoggedIn && (
         <>
-          <NavLink to="/signin">Log in</NavLink>
-          <NavLink to="/signup">Sign Up</NavLink>
+          <NavLink to="/signin">Connexion</NavLink>
+          <NavLink to="/signup">Inscription</NavLink>
         </>
       )}
     </nav>
