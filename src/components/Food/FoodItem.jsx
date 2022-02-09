c5a5c3e3e96adcad685134ba1cef3a105336d023
Mamadou AMADOU <doumstyle@gmail.com>
2022-02-09 10:26:27 +0100

    edit FoodItem and Foods components

--- src/components/Food/FoodItem.jsx
+++ src/components/Food/FoodItem.jsx
@@ -1,9 +1,35 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const FoodItem = ({ food, actions }) => {
+const FoodItem = ({ food, viewClbk, editClbk, deleteClbk }) => {
   return (
-    <div>
-      <h1>Food Item</h1>
+    <div className="box" key={food._id}>
+      <div className="left-items">
+        <div
+          className="catColor"
+          style={{ backgroundColor: food.category?.color }}
+        ></div>
+        <span className="food">{food.name}</span>
+      </div>
+      <div className="actionBtn">
+        <span>
+          <Link to={`/foods/food/view/${food._id}`}>
+            <i className="fa-solid fa-eye"></i>
+          </Link>
+        </span>
+        <span>
+          <Link to={`/foods/food/edit/${food._id}`}>
+            <i className="fa-solid fa-pencil"></i>
+          </Link>
+        </span>
+        <span>
+          <i
+            id={food._id}
+            className="fa-solid fa-trash-can"
+            onClick={deleteClbk}
+          ></i>
+        </span>
+      </div>
     </div>
   );
 };
--- src/pages/Foods.jsx
+++ src/pages/Foods.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import apiHandler from "../api/apiHandler";
+import FoodItem from "../components/Food/FoodItem";
 import "../styles/Foods.css";
 
 const Foods = () => {
@@ -34,34 +35,15 @@ const Foods = () => {
       {foods &&
         foods.map((food) => {
           return (
-            <div className="box" key={food._id}>
-              <div className="left-items">
-                <div
-                  className="catColor"
-                  style={{ backgroundColor: food.category?.color }}
-                ></div>
-                <span className="food">{food.name}</span>
-              </div>
-              <div className="actionBtn">
-                <span>
-                  <Link to={`/foods/food/view/${food._id}`}>
-                    <i className="fa-solid fa-eye"></i>
-                  </Link>
-                </span>
-                <span>
-                  <Link to={`/foods/food/edit/${food._id}`} action={"edit"}>
-                    <i className="fa-solid fa-pencil"></i>
-                  </Link>
-                </span>
-                <span>
-                  <i
-                    id={food._id}
-                    className="fa-solid fa-trash-can"
-                    onClick={handleDelete}
-                  ></i>
-                </span>
-              </div>
-            </div>
+            <>
+              <FoodItem
+                key={food._id}
+                food={food}
+                viewClbk
+                editClbk
+                deleteClbk={handleDelete}
+              />
+            </>
           );
         })}
     </div>
